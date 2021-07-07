a4aeef8228e589702e49ece72279feb179754edf
dmitriy-works <dmitriy-works@ukr.net>
2021-07-07 12:44:23 +0300

    finish project adaptive

--- app/js/script.js
+++ app/js/script.js
@@ -17,6 +17,42 @@ $(function(){
     prevArrow:'<button class="slick-arrow slick-prev"><img src="images/slide-left.svg" alt=""></button>',
     nextArrow:'<button class="slick-arrow slick-next"><img src="images/slide-right.svg" alt=""></button>',
 
+    responsive: [
+    {
+      breakpoint: 911,
+      settings: {
+        slidesToShow: 3,        
+        infinite: true,        
+      }
+    },
+
+    {
+      breakpoint: 681,
+      settings: {
+        slidesToShow: 2,        
+        infinite: true,        
+      }
+    },
+
+    {
+      breakpoint: 445,
+      settings: {
+        slidesToShow: 1,        
+        infinite: true,        
+      }
+    },
+        
+  ]
+  });
+
+  $('.header__menu-btn').on('click', function(){
+    $('.header__menu > ul').slideToggle();
+  });
+
+   $('.header__drop-down.drop-down').on('click', function(){
+    $(this).children('.drop-down__list').toggleClass('active');
+    $(this).children('.drop-down__link').toggleClass('active');
+   
   });
 
   $(".js-range-slider").ionRangeSlider({
